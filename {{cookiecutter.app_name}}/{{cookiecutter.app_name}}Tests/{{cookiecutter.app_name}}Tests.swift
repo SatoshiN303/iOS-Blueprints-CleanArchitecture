82aae98fd4d9f593b98f0f36f58807cdffdd10fa
shin.sato <shin.sato@i-studio.co.jp>
2020-09-08 19:10:30 +0900

    update bundleID settings

--- {{cookiecutter.app_name}}/{{cookiecutter.app_name}}Tests/{{cookiecutter.app_name}}Tests.swift
+++ {{cookiecutter.app_name}}/{{cookiecutter.app_name}}Tests/{{cookiecutter.app_name}}Tests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import __cookiecutter_app_name__
 
-class __cookiecutter_app_name__Tests: XCTestCase {
+class {{cookiecutter.app_name}}Tests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
