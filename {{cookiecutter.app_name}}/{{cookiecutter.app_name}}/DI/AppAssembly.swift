c833885fb0d6ed4c5b58295f6a2bca6f4c3f16f3
shin.sato <shin.sato@i-studio.co.jp>
2020-09-15 18:29:34 +0900

    Add DI container

---
+++ {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/DI/AppAssembly.swift
@@ -0,0 +1,29 @@
+//
+//  AppAssembly.swift
+//  {{cookiecutter.app_name}}
+//
+//  Copyright © 2020 {{cookiecutter.company_name}}. All rights reserved.
+//
+
+import Foundation
+import Swinject
+import SwinjectStoryboard
+
+class AppAssembly {
+    class var assembler: Assembler? {
+        return Assembler([])
+    }
+}
+
+extension SwinjectStoryboard {
+    @objc
+    class func setup() {
+        guard
+            let assembler = AppAssembly.assembler,
+            let container = assembler.resolver as? Container
+        else {
+            fatalError()
+        }
+        defaultContainer = container
+    }
+}
